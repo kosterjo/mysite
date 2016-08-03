196c74f8f2a69237cbff1cfd1513880a25a913b8
John Koster <kosterjo@umich.edu>
2016-08-03 15:12:53 -0700

    now using render() in index view

--- polls/views.py
+++ polls/views.py
@@ -10,7 +10,7 @@ def index(request):
 	context = {
 	  'latest_question_list': latest_question_list
 	}
-	return HttpResponse(template.render(context, request))
+	return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
 	return HttpResponse("you're looking at %s" % question_id)
