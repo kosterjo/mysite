b6c87f9d44de1e6979bc13a3839855b6b2a7d7a4
John Koster <kosterjo@umich.edu>
2016-08-04 13:16:43 -0700

    added detail view test class and gave it a test: test_detail_view_with_future_question

--- polls/tests.py
+++ polls/tests.py
@@ -103,3 +103,20 @@ class QuestionViewTests(TestCase):
 		  response.context['latest_question_list'],
 		  ['<Question: Past question 1>', '<Question: Past question 2>']
 		)
+
+
+class QuestionIndexDetailTests(TestCase):
+
+	def test_detail_view_with_future_question(self):
+		'''
+		detail view of an unpublished question 
+		should return 404
+		'''
+		future_question = create_question(question_text="Future question", days=30)
+		url = reverse('polls:detail', args=(future_question.id,))
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, 404)
+
+
+
+
