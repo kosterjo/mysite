80b482b9b7ca549991d35ef88442ad60fa0906cc
John Koster <kosterjo@umich.edu>
2016-08-03 17:20:55 -0700

    made views generic

--- polls/views.py
+++ polls/views.py
@@ -2,27 +2,29 @@ from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.core.urlresolvers import reverse 
+from django.views import generic 
 
 from .models import Question, Choice
 
-def index(request):
-	latest_question_list = get_list_or_404(Question)[:5]
-	context = {
-	  'latest_question_list': latest_question_list
-	}
-	return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/detail.html', {'question': question})
+	def get_queryset(self):
+		'''return last five questions'''
+		return get_list_or_404(Question)[:5]
 
-def results(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/results.html', {'question': question})
-	return HttpResponse("you just voted on %s" % question_id) 
+class DetailView(generic.DetailView):
+	model = Question 
+	template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+	model = Question 
+	template_name = 'polls/results.html'
 
 def vote(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
+
 	try:
 		selected_choice = question.choice_set.get(pk=request.POST['choice'])
 
